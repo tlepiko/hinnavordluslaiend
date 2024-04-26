7dadab8248f41e38caf3baefbf90dbd261ae60e8
tlepiko <60349843+tlepiko@users.noreply.github.com>
2024-04-27 00:03:41 +0300

    Päringute sisseviimine
    Sisestatud on päringud API-le. Laiend kuvab nüüd tootekoodi, soodsaimat hinda ning müüja nime.

--- popup.js
+++ popup.js
@@ -1,9 +1,40 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getGtin" }, function(response) {
-        if (response && response.gtin) {
-            document.getElementById("gtin").textContent = "Leitud GTIN on:" + response.gtin;
-        } else {
-            document.getElementById("gtin").textContent = "GTIN koodi ei leitud!";
-        }
+      if (response && response.gtin) {
+        document.getElementById("gtin").textContent = "Tootekood on: " +response.gtin;
+          fetch(`https://api.hinnavaatlus.ee/search/?query=${response.gtin}`)
+              .then(response => response.json())
+              .then(data1 => {
+                fetch(`https://api.hinnavaatlus.ee/product/${data1.data[0].id}`)
+                  .then(response => response.json())
+                  .then(data2 => {
+                      data2.offers.forEach(offer => {
+                        const price = parseFloat(offer.price);
+                        if (price == data1.data[0].price) {
+                          dealerName = offer.dealer.name;
+                      }
+                  });
+                      const price = data1.data[0].price;
+                      const shopName = dealerName;
+                      const priceElement = document.createElement('div');
+                      priceElement.textContent = `Hind: ${price}`;
+                      const shopElement = document.createElement('div');
+                      shopElement.textContent = `Müüja: ${shopName}`;
+                      const contentDiv = document.getElementById('andmed');
+                      contentDiv.appendChild(priceElement);
+                      contentDiv.appendChild(shopElement);
+                  })
+                  .catch(error => {
+                    console.error('Fetch error:', error);
+                });
+              })
+              .catch(error => {
+                  console.error('Fetch error:', error);
+              });
+  
+      } else {
+        document.getElementById("gtin").textContent = "Toodet ei leitud!";
+      }
     });
-});
+  });
+  
