45ab65102b1484ca25e77bb09098acb8c7acf2cc
tlepiko <60349843+tlepiko@users.noreply.github.com>
2024-04-26 23:27:46 +0300

    Tootekoodi kuvamine laiendis
    Laiend kuvab nüüd e-poe lehelt saadud tootekoodi.

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("running content.js");
-    if (request.action === "getTitle") {
-        const title = document.querySelector('title').textContent;
-        sendResponse({ title: title });
+    if (request.action === "getGtin") {
+      const webpageContent = document.documentElement.innerHTML;
+      const regex = /\b\d{13}\b/g;
+      const matches = webpageContent.match(regex);
+      const extractedIDs = [];
+      if (matches) {
+        matches.forEach(match => {
+            extractedIDs.push(match);
+        });
     }
-});
+      sendResponse({ gtin: extractedIDs[0] });
+      console.log("running content.js");
+    }
+  });
--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "getTitle" }, function(response) {
-        if (response && response.title) {
-            document.getElementById("title").textContent = "Esimese leitud tiitli sisu on:" + response.title;
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getGtin" }, function(response) {
+        if (response && response.gtin) {
+            document.getElementById("gtin").textContent = "Leitud GTIN on:" + response.gtin;
         } else {
-            document.getElementById("title").textContent = "Tiitlit ei leitud!";
+            document.getElementById("gtin").textContent = "GTIN koodi ei leitud!";
         }
     });
 });
