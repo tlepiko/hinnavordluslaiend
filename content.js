6c702d203c9db67878a676941dbe0eca6f00621c
tlepiko <60349843+tlepiko@users.noreply.github.com>
2024-04-28 22:54:05 +0300

    Lisaelemendid
    Lisatud nupud, pilt ja korrastatud välimust

--- content.js
+++ content.js
@@ -4,12 +4,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       const regex = /\b\d{13}\b/g;
       const matches = webpageContent.match(regex);
       const extractedIDs = [];
+      console.log(matches);
       if (matches) {
         matches.forEach(match => {
-            extractedIDs.push(match);
+            let sum = 0;
+            for (let i = 0; i < 12; i++) {
+                const digit = parseInt(match[i]);
+                sum += i %2 === 0 ? digit : digit * 3;
+                console.log(sum);
+            }
+            const checksum = (10 - (sum % 10)) % 10;
+            if (checksum === parseInt(match[12])) {
+                extractedIDs.push(match);
+            }
         });
     }
       sendResponse({ gtin: extractedIDs[0] });
-      console.log("running content.js");
+      console.log(extractedIDs);
     }
   });
--- popup.js
+++ popup.js
@@ -13,22 +13,51 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                         if (price == data1.data[0].price) {
                           dealerName = offer.dealer.name;
                           rating = offer.dealer.rating;
-                          console.log(rating);
+                          shopUrl = offer.shopUrl;
+                          img = data2.images[0];
+                          console.log(data2.images[0]);
                       }
                   });
                       const price = data1.data[0].price;
+                      const offersLink = `https://hinnavaatlus.ee/${data1.data[0].id}`;
                       const shopName = dealerName;
                       const shopRating = rating;
+                      const shopLink = shopUrl;
+                      const imgLink = img;
                       const priceElement = document.createElement('div');
                       priceElement.textContent = `Hind: ${price} €`;
                       const shopElement = document.createElement('div');
                       shopElement.textContent = `Müüja: ${shopName}`;
+                      const shopButton = document.createElement('button');
+                      shopButton.classList.add('btn', 'btn-primary');
+                      shopButton.textContent = 'Mine poodi';
+                      shopButton.onclick = function() {
+                        window.open(shopLink, '_blank');
+                      };
+                      const offersButton = document.createElement('button');
+                      offersButton.classList.add('btn', 'btn-secondary');
+                      offersButton.textContent = 'Vaata kõiki pakkumisi'
+                      offersButton.onclick = function() {
+                        window.open(offersLink, '_blank');
+                      }
                       const ratingElement = document.createElement('div');
                       ratingElement.textContent = `Müüja hinnang: ${shopRating}/5`;
                       const contentDiv = document.getElementById('andmed');
+
+                      const imageElement = document.createElement('img');
+                      imageElement.classList.add('card-img-top');
+                      imageElement.src = imgLink;
+                      imageElement.style.maxWidth = '80%';
+                      
+                      contentDiv.appendChild(imageElement);
                       contentDiv.appendChild(priceElement);
                       contentDiv.appendChild(shopElement);
+                      contentDiv.appendChild(shopButton);
+                      contentDiv.appendChild(offersButton);
                       contentDiv.appendChild(ratingElement);
+                      contentDiv.style.display = 'flex';
+                      contentDiv.style.alignItems = 'center';
+                      
                   })
                   .catch(error => {
                     console.error('Fetch error:', error);
