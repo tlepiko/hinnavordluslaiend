033ed52e6bdfb15b1385de8dc39662fc84ba600d
tlepiko <60349843+tlepiko@users.noreply.github.com>
2024-04-29 01:26:26 +0300

    Kasutajaliidese uuendused
    Lisatud taustavärv ja muudetud paigutust

--- popup.js
+++ popup.js
@@ -1,7 +1,20 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getGtin" }, function(response) {
       if (response && response.gtin) {
-        document.getElementById("gtin").textContent = "Tootekood on: " +response.gtin;
+        const contentDiv = document.getElementById('andmed');
+        const searchButton = document.createElement('button');
+        const searchLink = `https://google.com/search?q=${response.gtin}`;
+        searchButton.classList.add('btn', 'btn-info');
+        searchButton.style.width = '80%';
+        searchButton.style.marginBottom = '5px';
+        searchButton.textContent = 'Otsi toodet internetist';
+        searchButton.onclick = function() {
+            window.open(searchLink, '_blank');
+        }
+        const productCode = document.createElement(`p`);
+        productCode.textContent = `Toodekood: ` + response.gtin;
+        const notFound = document.createElement('strong');
+        notFound.textContent = `Toodet ei leitud!`
           fetch(`https://api.hinnavaatlus.ee/search/?query=${response.gtin}`)
               .then(response => response.json())
               .then(data1 => {
@@ -29,34 +42,39 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                       const shopElement = document.createElement('div');
                       shopElement.textContent = `Müüja: ${shopName}`;
                       const shopButton = document.createElement('button');
+                      shopButton.style.width = '80%';
+                      shopButton.style.marginTop = '5px';
                       shopButton.classList.add('btn', 'btn-primary');
                       shopButton.textContent = 'Mine poodi';
                       shopButton.onclick = function() {
                         window.open(shopLink, '_blank');
                       };
                       const offersButton = document.createElement('button');
+                      offersButton.style.width = '80%';
+                      offersButton.style.margin = '5px';
                       offersButton.classList.add('btn', 'btn-secondary');
-                      offersButton.textContent = 'Vaata kõiki pakkumisi'
+                      offersButton.textContent = 'Vaata kõiki pakkumisi';
                       offersButton.onclick = function() {
                         window.open(offersLink, '_blank');
                       }
                       const ratingElement = document.createElement('div');
                       ratingElement.textContent = `Müüja hinnang: ${shopRating}/5`;
-                      const contentDiv = document.getElementById('andmed');
+                      
 
                       const imageElement = document.createElement('img');
                       imageElement.classList.add('card-img-top');
                       imageElement.src = imgLink;
                       imageElement.style.maxWidth = '80%';
+                      imageElement.style.margin = '5px';
                       
                       contentDiv.appendChild(imageElement);
                       contentDiv.appendChild(priceElement);
                       contentDiv.appendChild(shopElement);
+                      contentDiv.appendChild(ratingElement);
                       contentDiv.appendChild(shopButton);
                       contentDiv.appendChild(offersButton);
-                      contentDiv.appendChild(ratingElement);
-                      contentDiv.style.display = 'flex';
-                      contentDiv.style.alignItems = 'center';
+                      contentDiv.appendChild(searchButton);
+                      document.getElementById("gtin").appendChild(productCode);
                       
                   })
                   .catch(error => {
@@ -64,7 +82,9 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                 });
               })
               .catch(error => {
-                  console.error('Fetch error:', error);
+                document.getElementById("gtin").appendChild(notFound);
+                document.getElementById("gtin").appendChild(searchButton);
+                document.getElementById("gtin").appendChild(productCode);
               });
   
       } else {
